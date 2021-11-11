d02ca54883ca16967e00c35a0d9741a9cbe22ba8
garciadelcastillo <personal@garciadelcastillo.es>
2021-11-11 07:40:49 -0500

    More sample files

---
+++ Advanced_Development_Grasshopper/PersonClass/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonClass
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool IsProfessor;
+
+        public Person(string n, int a, bool ip)
+        {
+            this.Name = n;
+            this.Age = a;
+            this.IsProfessor = ip;
+        }
+
+        public override string ToString()
+        {
+            return this.Name + ", age: " + this.Age + " is professor? " + this.IsProfessor;
+        }
+    }
+}
