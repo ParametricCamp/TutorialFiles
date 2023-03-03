c92adb663c2aae075a34fa7af72246540afe0721
garciadelcastillo <personal@garciadelcastillo.es>
2023-03-03 12:32:08 -0500

    Bitwise operators

---
+++ Learning_C#/64_BitwiseOperators/64_bitwsie-operators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64_bitwsie_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bitAND = 128 & 64;  // 0
+            Console.WriteLine("128 & 64: " + bitAND);
+            bitAND = 110 & 53;
+            Console.WriteLine("110 & 53: " + bitAND);
+            Console.WriteLine();
+
+            int bitOR = 128 | 64;   // 192
+            Console.WriteLine("128 | 64: " + bitOR);
+            bitOR = 110 | 53;
+            Console.WriteLine("110 | 53: " + bitOR);
+            Console.WriteLine();
+
+            int bitSHIFT = 1 << 7;  // 128
+            Console.WriteLine("1   << 7: " + bitSHIFT);
+            bitSHIFT >>= 4;
+            Console.WriteLine("128 >> 4: " + bitSHIFT);
+
+            bitSHIFT = 5 << 3;
+            Console.WriteLine("5   << 3: " + bitSHIFT);
+            bitSHIFT >>= 1;
+            Console.WriteLine("40  >> 1: " + bitSHIFT);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Learning_C#/64_BitwiseOperators/64_bitwsie-operators/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("64_bitwsie-operators")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("64_bitwsie-operators")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fd9d7474-1776-45ef-91b8-9ce10fb648b6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
